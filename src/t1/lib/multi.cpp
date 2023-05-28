921b2513005945d61cf31494ed72b2170514b240
Noemi1313 <51099544+Noemi1313@users.noreply.github.com>
2023-05-28 17:58:57 -0600

    Update multi.cpp

--- src/t1/lib/multi.cpp
+++ src/t1/lib/multi.cpp
@@ -4,13 +4,17 @@
 #include <iostream>
 
 extern "C"{
+    // Funcion para multiplicar 2 numeros *100
     int* multi(int x, int y){
+        // Crear un arreglo dinamico de ints para guardar los resultados
         int*c = new int[2];
+        // Multiplicar los valores y guardar los resultados en el arreglo
         c[0] = x*100;
         c[1] = y*100;
         return c;
     }
-
+    
+    // Funcion para borrar el arreglo
     void delete_arr(int* ptr){
         delete[] ptr;
     }
