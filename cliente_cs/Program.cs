0fe89874cc1bf2b7f42a286b44b95810870c9727
Noemi1313 <51099544+Noemi1313@users.noreply.github.com>
2023-05-28 17:54:27 -0600

    Update Program.cs

--- cliente_cs/Program.cs
+++ cliente_cs/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading;
+using System.Threading;
 using System.Diagnostics;
 using Google.Protobuf;
 using Grpc.Core;
@@ -12,12 +12,17 @@ namespace Application
     {
         static void Main(string[] args)
         {
+            // Crear canal gRPC para establecer una conexion con el servidor
             var channel = new Grpc.Core.Channel("127.0.0.1:7042", ChannelCredentials.Insecure);
+            // Crear el cliente gRPC 
             var client = new RPC.RPCClient(channel);
             while (true)
             {
+                // Hacer una peticion gRPC al servidor para obtener las coordenadas
                 var result = client.GetCoord(new Google.Protobuf.WellKnownTypes.Empty());
+                // Realizar el display de los valores
                 Console.WriteLine("Coordenadas: " + result.Values[0] + ", " + result.Values[1]);
+                // Realizar una peticion cada medio segundo
                 Thread.Sleep(500);
             }
         }
