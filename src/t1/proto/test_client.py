f926af2f946112f5fb0835ff45915930f62082bf
Noemi1313 <51099544+Noemi1313@users.noreply.github.com>
2023-05-28 20:08:41 -0600

    Update test_client.py

--- src/t1/proto/test_client.py
+++ src/t1/proto/test_client.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+/**
+
+@file client.py
+@brief Cliente gRPC para la solicitud de coordenadas
+Implementacion de un cliente gRPC que realiza una solicitud al servidor gRPC
+para obtener las coordenadas actuales. Utiliza el paquete rpc_pb2_grpc y google.protobuf.empty_pb2
+generados a partir del archivo rpc.proto.
+El cliente establece una conexión con el servidor, realiza la solicitud y muestra la respuesta recibida.
+El servidor debe estar en ejecución y escuchando en la dirección y puerto especificados.
+@author Noemi Carolina Guerra Montiel 
+@date Fecha de creación: 29/05/2023
+*/
+
 import grpc
 from concurrent import futures
 import google.protobuf.empty_pb2 as empty_pb2
@@ -8,6 +21,10 @@ import signal
 import rpc_pb2_grpc as pb2_grpc
 
 def run():
+    """
+    @brief Función principal del cliente gRPC.
+    """
+    
     # Create a gRPC channel to connect to the server
     channel = grpc.insecure_channel('localhost:7042')  # Replace with the server address and port
 
